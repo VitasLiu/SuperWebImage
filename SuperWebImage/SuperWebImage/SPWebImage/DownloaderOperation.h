f658ae8c6f83b32a012272e67512a0efb6c564ae
VitasLiu <vitas_liu@163.com>
2016-10-23 13:31:11 +0800

    创建自定义操作类

---
+++ SuperWebImage/SuperWebImage/SPWebImage/DownloaderOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.h
+//  SuperWebImage
+//
+//  Created by Vitas on 2016/10/23.
+//  Copyright © 2016年 Vitas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloaderOperation : NSOperation
+
+@end
---
+++ SuperWebImage/SuperWebImage/SPWebImage/DownloaderOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.m
+//  SuperWebImage
+//
+//  Created by Vitas on 2016/10/23.
+//  Copyright © 2016年 Vitas. All rights reserved.
+//
+
+#import "DownloaderOperation.h"
+
+@implementation DownloaderOperation
+
+@end
